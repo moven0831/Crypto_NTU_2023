181c9389a23299d19cd3ada807d12948fddc13f0
Moven Tsai <moven0831@gmail.com>
2023-05-24 15:33:56 +0800

    check ECDSA Signer follows RFC 6979 on the rules of random ephemeral key generation

--- ecdsa_ds/src/main.rs
+++ ecdsa_ds/src/main.rs
@@ -1,4 +1,5 @@
-use k256::{ecdsa::SigningKey, ecdsa::{VerifyingKey, Signature}, ecdsa::signature::Verifier, elliptic_curve::bigint::{U256, Encoding, Uint}, NonZeroScalar, FieldBytes, schnorr::signature::RandomizedSigner};
+use hex_literal::hex;
+use k256::{ecdsa::{SigningKey, Signature, signature::RandomizedSigner, VerifyingKey, signature::Verifier}, elliptic_curve::bigint::Uint, NonZeroScalar, FieldBytes};
 use rand::rngs::OsRng;
 
 fn main() {
@@ -9,20 +10,18 @@ fn main() {
     
     // Step 1: Generate a private key from a specific integer and the corresponding public key.
     let private_key: SigningKey = SigningKey::from_bytes(&secret_bytes).unwrap();
-    // let private_key: SigningKey = SigningKey::from_bytes(secret_bytes.as_slice().into()).unwrap();
     let public_key: VerifyingKey = VerifyingKey::from(&private_key);
 
-    // Step 2: Sign the message.
-    let hash_val: U256 = U256::from_be_hex("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
-    let hash_bytes = hash_val.to_be_bytes();
+    // Step 2: Sign the message. RandomizedSigner follows RFC 6979 for random ephermeral key.
+    let hash_bytes = hex!("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
     let signature: Signature = private_key.sign_with_rng(&mut OsRng, &hash_bytes);  // Sign with a random ephermeral key
     
     // Step 3: Print out Public key and signature
-    println!("Public key: {:?}", public_key.to_encoded_point(false).to_string());
-    println!("Signature (with a random ephermeral key):\n----------> {:?}", signature.to_string());
+    println!("\nPublic key: {:?}", public_key.to_encoded_point(false).to_string());
+    println!("\nSignature (with a random ephermeral key):\n----------> {:?}", signature.to_string());
     
     // Step 4: Verify the signature using the public key.
     assert!(public_key.verify(&hash_bytes, &signature).is_ok());
-    println!("(pass) Verify the signature using the public key");
+    println!("\n(pass) Verify the signature using the public key");
     
 }
